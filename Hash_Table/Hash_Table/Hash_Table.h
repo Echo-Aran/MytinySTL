4f6cd62f3772ab6082d6ff4d72295facbd58c555
Echo <wxr20040725@qq.com>
2023-09-24 00:31:07 +0800

    Update HashTable

--- Hash_Table/Hash_Table/Hash_Table.h
+++ Hash_Table/Hash_Table/Hash_Table.h
@@ -2,25 +2,9 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-//哈希表里存储键值对
-enum State
-{
-	Delete,
-	Empty,
-	Exist
-};
-
-template<class K,class V>
-struct HashData
-{
-	pair<K, V> _kv;
-	State _state = Empty;
-};
-
-
 
 template<class K>
-struct HashFunc
+struct DefaultHashFunc
 {
 	size_t operator()(const K& key)
 	{
@@ -30,7 +14,7 @@ struct HashFunc
 
 //特化
 template<>
-struct HashFunc<string>
+struct DefaultHashFunc<string>
 {
 	size_t operator()(const string& str)
 	{
@@ -45,84 +29,256 @@ struct HashFunc<string>
 	}
 };
 
-
-
-
-template<class K, class V>
-class Hash_table
+//开放寻址法
+namespace open_address
 {
-	size_t _n = 0;
-	vector<HashData<K, V>> _table;
-public:
-	Hash_table()
-		:_n(10)
+	//哈希表里存储键值对
+	enum State
 	{
-		_table.resize(10);
-	}
+		Delete,
+		Empty,
+		Exist
+	};
 
-	bool Insert(const pair<K, V>& kv)
+	template<class K, class V>
+	struct HashData
 	{
-		//扩容
-		if (_n * 10 / _table.size() > 7)
+		pair<K, V> _kv;
+		State _state = Empty;
+	};
+
+
+	template<class K, class V, class HashFunc = DefaultHashFunc<K>>
+	class Hash_table
+	{
+		size_t _n = 0;
+		vector<HashData<K, V>> _table;
+	public:
+		Hash_table()
+			:_n(10)
 		{
-			size_t newsize = _table.size() * 2;
-			Hash_table<K, V> newTable;
-			newTable._table.resize(newsize);
+			_table.resize(10);
+		}
 
-			//遍历旧表的数据插入到新表
-			for (size_t i = 0; i < _table.size(); i++)
+		bool Insert(const pair<K, V>& kv)
+		{
+			//扩容
+			if (_n * 10 / _table.size() > 7)
 			{
-				if (_table[i]._state == Exist)
+				size_t newsize = _table.size() * 2;
+				Hash_table<K, V> newTable;
+				newTable._table.resize(newsize);
+
+				//遍历旧表的数据插入到新表
+				for (size_t i = 0; i < _table.size(); i++)
 				{
-					newTable.Insert(_table[i]._kv);
+					if (_table[i]._state == Exist)
+					{
+						newTable.Insert(_table[i]._kv);
+					}
 				}
+
+				_table.swap(newTable._table);
 			}
 
-			_table.swap(newTable._table);
-		}
-
-		//线性探测
-		HashFunc<K> hf;
-		size_t hashi = hf(kv.first) % _table.size();
-		while (_table[hashi]._state == Exist)
-		{
-			++hashi;
-			hashi %= _table.size();
-		}
-		_table[hashi]._kv = kv;
-		_table[hashi]._state = Exist;
-		++_n;
-		return true;
-	}
-
-	HashData<const K, V>* Find(const K& key)const
-	{
-		HashFunc<K> hf;
-		size_t hashi = hf(key) % _table.size();
-		while (_table[hashi]._state != Empty)
-		{
-			if (_table[hashi]._state == Exist && _table[hashi]._kv.first == key)
+			//线性探测
+			HashFunc hf;
+			size_t hashi = hf(kv.first) % _table.size();
+			while (_table[hashi]._state == Exist)
 			{
-				return (HashData<const K, V>*) & _table[hashi];
+				++hashi;
+				hashi %= _table.size();
 			}
-			++hashi;
-			hashi %= _table.size();
-		}
-		return nullptr;
-	}
-
-	bool Erase(const K& key)
-	{
-		HashData<const K, V>* ret = Find(key);
-		if (ret)
-		{
-			ret->_state = Delete;
-			--_n;
-
+			_table[hashi]._kv = kv;
+			_table[hashi]._state = Exist;
+			++_n;
 			return true;
 		}
 
-		return false;
-	}
+		HashData<const K, V>* Find(const K& key)const
+		{
+			HashFunc hf;
+			size_t hashi = hf(key) % _table.size();
+			while (_table[hashi]._state != Empty)
+			{
+				if (_table[hashi]._state == Exist && _table[hashi]._kv.first == key)
+				{
+					return (HashData<const K, V>*) & _table[hashi];
+				}
+				++hashi;
+				hashi %= _table.size();
+			}
+			return nullptr;
+		}
 
-};
+		bool Erase(const K& key)
+		{
+			HashData<const K, V>* ret = Find(key);
+			if (ret)
+			{
+				ret->_state = Delete;
+				--_n;
+
+				return true;
+			}
+
+			return false;
+		}
+
+	};
+}
+
+namespace hash_bucket
+{
+	template<class K,class V>
+	struct HashNode
+	{
+		pair<K, V> _kv;
+		HashNode* _next;
+
+		HashNode(const pair<K, V>& kv)
+			:_kv(kv)
+			,_next(nullptr)
+		{}
+	};
+
+	template<class K,class V,class HashFunc=DefaultHashFunc<K>>
+	class HashTable
+	{
+		typedef HashNode<K, V> Node;
+
+		size_t _n = 0;//存储多少有效数据
+		vector<Node*> _table;//指针数组
+	public:
+		HashTable()
+		{
+			_table.resize(10, nullptr);
+		}
+
+		~HashTable()
+		{
+			for (size_t i = 0; i < _table.size(); i++)
+			{
+				Node* cur = _table[i];
+				while (cur)
+				{
+					Node* next = cur->_next;
+					delete cur;
+					cur = next;
+				}
+				_table[i] = nullptr;
+			}
+		}
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			if (Find(kv.first)) 
+			{
+				return false;
+			}
+
+			HashFunc hf;
+
+			//当负载因子==_table.size()，扩容
+			if (_n == _table.size())
+			{
+				size_t newSize = _table.size() * 2;
+				vector<Node*> newTable;
+				newTable.resize(newSize, nullptr);
+
+				//遍历旧表，改变指针直系那个，顺手牵羊
+				for (size_t i = 0; i < _table.size(); i++)
+				{
+					Node* cur = _table[i];
+					while (cur)
+					{
+						Node* next = cur->_next;
+
+						// 头插到新表
+						size_t hashi = hf(cur->_kv.first) % newSize;
+						cur->_next = nullptr;
+						newTable[hashi] = cur;
+
+						cur = next;
+					}
+					_table[i] = nullptr;
+				}
+				//将表交换
+				_table.swap(newTable);
+			}
+
+			size_t hashi = hf(kv.first) % _table.size();
+			// 头插
+			Node* newnode = new Node(kv);
+			newnode->_next = _table[hashi];
+			_table[hashi] = newnode;
+			++_n;
+			return true;
+
+		}
+
+		Node* Find(const K& key)const
+		{
+			HashFunc hf;
+			size_t hashi = hf(key) % _table.size();
+
+			Node* cur = _table[hashi];
+			while (cur)
+			{
+				if (cur->_kv.first == key)
+				{
+					return cur;
+				}
+				else
+				{
+					cur = cur->_next;
+				}
+			}
+			return nullptr;
+		}
+
+		bool Erase(const K& key)
+		{
+			HashFunc hf;
+			size_t hashi = hf(key) % _table.size();
+			Node* prev = nullptr;
+			Node* cur = _table[hashi];
+			while (cur)
+			{
+				if (cur->_kv.first == key)
+				{
+					if (prev == nullptr)
+					{
+						_table[hashi] = cur->_next;
+					}
+					else
+					{
+						prev->_next = cur->_next;
+					}
+
+					delete cur;
+					return true;
+				}
+				prev = cur;
+				cur = cur->_next;
+			}
+			return false;
+		}
+
+		void Print()
+		{
+			for (size_t i = 0; i < _table.size(); i++)
+			{
+				printf("[%d]->", i);
+				Node* cur = _table[i];
+				while (cur)
+				{
+					cout << cur->_kv.first << ":" << cur->_kv.second << "->";
+					cur = cur->_next;
+				}
+				printf("NULL\n");
+			}
+			cout << endl;
+		}
+	};
+}
--- Hash_Table/Hash_Table/test.cpp
+++ Hash_Table/Hash_Table/test.cpp
@@ -3,40 +3,118 @@
 #include<string>
 using namespace std;
 
+//int main()
+//{
+//	open_address::Hash_table<int, int> ht;
+//	int a[] = { 1,111,4,7,15,25,44,9 };
+//	for (auto e : a)
+//	{
+//		ht.Insert(make_pair(e, e));
+//	}
+//
+//	ht.Erase(15);
+//
+//	auto ret = ht.Find(4);
+//	//ret->_kv.first = 41;
+//	ret->_kv.second = 400;
+//
+//	//HashTable<string, string, StringHashFunc> dict;
+//	open_address::Hash_table<string, string> dict;
+//	dict.Insert(make_pair("sort", "ÅÅÐò"));
+//	dict.Insert(make_pair("left", "xxx"));
+//	auto dret = dict.Find("left");
+//	//dret->_kv.first = "xx";
+//	dret->_kv.second = "×ó±ß";
+//
+//	string s1("xxx");
+//	string s2("xxx");
+//
+//
+//	/*HashFunc<string> hf;
+//	cout << hf(s1) << endl;
+//	cout << hf(s2) << endl;
+//	cout << hf("bacd") << endl;
+//	cout << hf("abcd") << endl;
+//	cout << hf("abbe") << endl;
+//	cout << hf("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=65081411_1_oem_dg&wd=STATE&fenlei=256&rsv_pq=0xdd48647300054f47&rsv_t=1cd5rO%2BE6TJzo6qf9QKcibznhQ9J3lFwGEzmkc0Goazr3HuQSIIc2zD78Pt0&rqlang=en&rsv_enter=1&rsv_dl=tb&rsv_sug3=2&rsv_n=2&rsv_sug1=1&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&prefixsug=STAT%2526gt%253B&rsp=5&inputT=656&rsv_sug4=796") << endl;*/
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	open_address::HashTable<int, int> ht;
+//	int a[] = { 1,111,4,7,15,25,44,9 };
+//	for (auto e : a)
+//	{
+//		ht.Insert(make_pair(e, e));
+//	}
+//
+//	ht.Erase(15);
+//
+//	auto ret = ht.Find(4);
+//	//ret->_kv.first = 41;
+//	ret->_kv.second = 400;
+//
+//	//HashTable<string, string, StringHashFunc> dict;
+//	open_address::HashTable<string, string> dict;
+//	dict.Insert(make_pair("sort", "ÅÅÐò"));
+//	dict.Insert(make_pair("left", "xxx"));
+//	auto dret = dict.Find("left");
+//	//dret->_kv.first = "xx";
+//	dret->_kv.second = "×ó±ß";
+//
+//	string s1("xxx");
+//	string s2("xxx");
+//
+//
+//	open_address::DefaultHashFunc<string> hf;
+//	cout << hf(s1) << endl;
+//	cout << hf(s2) << endl;
+//	cout << hf("bacd") << endl;
+//	cout << hf("abcd") << endl;
+//	cout << hf("abbe") << endl;
+//	cout << hf("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=65081411_1_oem_dg&wd=STATE&fenlei=256&rsv_pq=0xdd48647300054f47&rsv_t=1cd5rO%2BE6TJzo6qf9QKcibznhQ9J3lFwGEzmkc0Goazr3HuQSIIc2zD78Pt0&rqlang=en&rsv_enter=1&rsv_dl=tb&rsv_sug3=2&rsv_n=2&rsv_sug1=1&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&prefixsug=STAT%2526gt%253B&rsp=5&inputT=656&rsv_sug4=796") << endl;
+//
+//	return 0;
+//}
+
 int main()
 {
-	Hash_table<int, int> ht;
+	hash_bucket::HashTable<int, int> ht;
 	int a[] = { 1,111,4,7,15,25,44,9 };
 	for (auto e : a)
 	{
 		ht.Insert(make_pair(e, e));
 	}
+	ht.Print();
 
-	ht.Erase(15);
+	ht.Insert(make_pair(14, 14));
+	ht.Print();
 
-	auto ret = ht.Find(4);
-	//ret->_kv.first = 41;
-	ret->_kv.second = 400;
+	ht.Insert(make_pair(24, 24));
+	ht.Print();
 
-	//HashTable<string, string, StringHashFunc> dict;
-	Hash_table<string, string> dict;
+	ht.Insert(make_pair(34, 34));
+	ht.Print();
+
+	ht.Erase(44);
+	ht.Erase(4);
+	ht.Erase(24);
+	ht.Print();
+
+	hash_bucket::HashTable<string, string> dict;
 	dict.Insert(make_pair("sort", "ÅÅÐò"));
 	dict.Insert(make_pair("left", "xxx"));
+	dict.Insert(make_pair("insert", "²åÈë"));
+	dict.Insert(make_pair("string", "×Ö·û´®"));
+	dict.Insert(make_pair("bucket", "Í°"));
+
 	auto dret = dict.Find("left");
 	//dret->_kv.first = "xx";
 	dret->_kv.second = "×ó±ß";
-
-	string s1("xxx");
-	string s2("xxx");
-
-
-	/*HashFunc<string> hf;
-	cout << hf(s1) << endl;
-	cout << hf(s2) << endl;
-	cout << hf("bacd") << endl;
-	cout << hf("abcd") << endl;
-	cout << hf("abbe") << endl;
-	cout << hf("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=65081411_1_oem_dg&wd=STATE&fenlei=256&rsv_pq=0xdd48647300054f47&rsv_t=1cd5rO%2BE6TJzo6qf9QKcibznhQ9J3lFwGEzmkc0Goazr3HuQSIIc2zD78Pt0&rqlang=en&rsv_enter=1&rsv_dl=tb&rsv_sug3=2&rsv_n=2&rsv_sug1=1&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&prefixsug=STAT%2526gt%253B&rsp=5&inputT=656&rsv_sug4=796") << endl;*/
+	dict.Print();
 
 	return 0;
 }
